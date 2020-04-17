7a8c6f1a5546ca64b002a6e60a93791665077bed
pawnxia <pawncd@cn.ibm.com>
2020-04-17 18:38:19 +0800

    Initial commit

---
+++ src/main/java/com/thread/Test.java
@@ -0,0 +1,27 @@
+package com.thread;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread("Thread-pawn") {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        t1.start();
+        ThreadGroup tg = Thread.currentThread().getThreadGroup();
+
+        Thread[] arrs = new Thread[tg.activeCount()];
+        tg.enumerate(arrs);
+
+        Arrays.asList(arrs).forEach(System.out::println);
+
+    }
+}
---
+++ src/main/java/com/thread/bank/Worker.java
@@ -0,0 +1,32 @@
+package com.thread.bank;
+
+public class Worker implements Runnable{
+
+    private static final int MAX =50;
+
+    private static int index = 1;
+
+    @Override
+    public void run() {
+
+            while (index <= MAX) {
+                synchronized (this) {
+                    System.out.println(Thread.currentThread() + " 拿到的号码 " + index);
+                    index++;
+                }
+            }
+    }
+
+    public static void main(String[] args) {
+        final Worker w = new Worker();
+        Thread w1 = new Thread(w,"");
+        w1.setName("柜员1");
+        Thread w2 = new Thread(w);
+        w2.setName("柜员2");
+        Thread w3 = new Thread(w);
+        w3.setName("柜员3");
+        w1.start();
+        w2.start();
+        w3.start();
+    }
+}
