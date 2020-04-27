bacaf91534fa39b01a3b334186cb903a6511aea7
pawnxia <pawncd@cn.ibm.com>
2020-04-27 15:55:49 +0800

    commit3

---
+++ src/main/java/com/thread/concurence/Hashmap1.java
@@ -0,0 +1,11 @@
+package com.thread.concurence;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Hashmap1 {
+
+    public static void main(String[] args) {
+        Map map = new ConcurrentHashMap();
+    }
+}
